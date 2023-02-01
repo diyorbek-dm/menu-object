3b2434a738edd629c0e2e49269bc141e58dbcdb1
diyorbek-dm <123005290+diyorbek-dm@users.noreply.github.com>
2023-02-02 02:40:03 +0500

    Add files via upload

---
+++ js2.js
@@ -0,0 +1,19 @@
+const obj = receipt()
+const delivery = 9000 + ' сум'
+let count = 0
+let sum = 0
+
+for (const key in obj) {
+  var price = obj[key].price
+  let info = obj[key].info
+  count++
+
+  for (let i = 0; i < 1; i++) {
+    sum += price
+  }
+
+  console.log(`Заказ ${count}: ${key}, ${info}, ${price} сум`)
+}
+
+console.log('---------- К оплате ----------')
+console.log(sum + ' сум ' + ' Доствака: ' + delivery)
